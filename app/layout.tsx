12920b3ae1bfac337d0055092c96173764e17702
Adrian Palcic <adrian.palcic@gmail.com>
2025-10-26 13:48:59 +0100

    global button and text styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,55 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
 
 export const metadata: Metadata = {
   title: "Tipsy Muse | Wine & Paint",
   description: "Wine & Paint",
 };
+
+const luckiestGuy = localFont({
+  src: [
+    {
+      path: "./fonts/LuckiestGuy-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-luckiest-guy",
+  display: "swap",
+});
+
+const baloo = localFont({
+  src: [
+    {
+      path: "./fonts/Baloo2-Bold.ttf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Baloo2-ExtraBold.ttf",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Baloo2-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Baloo2-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Baloo2-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+  ],
+  variable: "--font-baloo",
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -13,8 +57,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+    <html lang="en" className={`${luckiestGuy.variable} ${baloo.variable}`}>
+      <body suppressHydrationWarning className={`antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1 className="text-3xl">Hello Tipsy Muse</h1>;
+  return (
+    <section id="hero" className="section-overlay-a py-40 px-10 flex gap-10">
+      <button className="button blue">Radionice</button>
+      <button className="button red">Radionice</button>
+      <button className="button yellow">Radionice</button>
+    </section>
+  );
 }

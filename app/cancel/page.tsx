b793db17611a6be3a5ad3419883941bc20214696
Adrian Palcic <adrian.palcic@gmail.com>
2025-11-17 13:12:55 +0100

    Success and cancel payment routes

--- app/cancel/page.tsx
+++ app/cancel/page.tsx
@@ -1,7 +1,28 @@
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
-  return <h1>Uplata otkazana.</h1>;
+  return (
+    <main>
+      <section className="section-overlay-a section-padding pt-44">
+        <h1 className="title shadow-blue-text text-secondary text-center">
+          Otkazana Uplata
+        </h1>
+        <p className="subtitle text-center">
+          Ili ste otkazali uplatu ili je nešto pošlo po krivu. Svakako
+          pogledajte ostatak naših radionica
+        </p>
+        <div className="w-full flex justify-center gap-6">
+          <Link href="/radionice">
+            <button className="button blue">Radionice</button>
+          </Link>
+          <Link href="/naslovna">
+            <button className="button red">Naslovna</button>
+          </Link>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default page;
--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
-  return <h1>Uplata uspješna! 🎉</h1>;
+  return (
+    <main>
+      <section className="section-overlay-a section-padding pt-44">
+        <h1 className="title shadow-pink-text text-center">Uspješna Uplata!</h1>
+        <p className="subtitle text-center">
+          Na mail primate potvrdu o uplati, to je Vaša ulaznica
+        </p>
+        <div className="w-full flex justify-center">
+          <Link href="/naslovna">
+            <button className="button blue">Povratak na Naslovnu</button>
+          </Link>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default page;
